abf7cdf3231c33845252610814efd14987c325c9
Lamtalsi Anissa <lamtalsi.03@gmail.com>
2024-07-18 23:20:34 +0200

    create react-app argentbank plus créations des pages et du routage

---
+++ argentbank/src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+//import des pages
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+import Profile from "./pages/Profile/Profile";
+
+//import des composants
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
+function App() {
+  return (
+    <Router>
+      {/*gérer la route et la navigation*/}
+      {/* Header présent dans toutes les pages */}
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+      {/* Footer présent dans toutes les pages */}
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
--- server/database/connection.js
+++ server/database/connection.js
@@ -1,13 +1,45 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const databaseUrl =
-  process.env.DATABASE_URL || 'mongodb://localhost/argentBankDB'
+  process.env.DATABASE_URL || "mongodb://localhost/argentBankDB";
 
 module.exports = async () => {
   try {
-    await mongoose.connect(databaseUrl, { useNewUrlParser: true })
-    console.log('Database successfully connected')
+    await mongoose.connect(
+      "mongodb+srv://lamtalsi03:G5b2N4XUNPtEff51@cluster0.u2d9oay.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+      { useNewUrlParser: true }
+    );
+    console.log("Database successfully connected");
   } catch (error) {
-    console.error(`Database Connectivity Error: ${error}`)
-    throw new Error(error)
+    console.error(`Database Connectivity Error: ${error}`);
+    throw new Error(error);
+  }
+};
+
+/*const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://lamtalsi03:G5b2N4XUNPtEff51@cluster0.u2d9oay.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
   }
 }
+run().catch(console.dir);*/
