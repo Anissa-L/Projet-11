239bf10649bc018e99338521c793cef0dc415ba3
Lamtalsi Anissa <lamtalsi.03@gmail.com>
2024-07-23 19:48:44 +0200

    modification header pour la page profile

--- argentbank/src/components/Header/Header.jsx
+++ argentbank/src/components/Header/Header.jsx
@@ -1,21 +1,36 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation} from "react-router-dom";
 import "./Header.scss";
 import logo from "../../imgReact/argentBankLogo.png"; // Importez l'image
 
 
 function Header (){
+    const location = useLocation();
+
     return(
         <header className="main-nav">
             <NavLink to="/" className="main-nav-logo">
                 <img src={logo} alt="Argent Bank Logo" className="main-nav-logo-image"/>
                 <h1 className="sr-only">Argent Bank</h1>
             </NavLink>
-            <NavLink to="/login" className="main-nav-item">
-            <i className="fa fa-user-circle main-nav-icon-user"></i>
-            Sign In
-            </NavLink>
-
+            {location.pathname === "/profile" ? (
+                <div>
+                <NavLink to="/profile" className="main-nav-item main-nav-item-user">
+                    <i className="fa fa-user-circle main-nav-icon-user"></i>
+                    Tony
+                </NavLink>
+                <NavLink to="/" className="main-nav-item">
+                <i className="fa fa-sign-out main-nav-icon-sign-out"></i>
+                Sign Out
+                </NavLink>
+                </div>
+            ) : (
+                <NavLink to="/login" className="main-nav-item">
+                <i className="fa fa-user-circle main-nav-icon-user"></i>
+                Sign In
+                </NavLink>
+            )}
+            
         </header>
     );
 }
